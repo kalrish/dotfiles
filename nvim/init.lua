a69f4548bfcfa21bed903eed97088c45f97f8fee
David Joaquín Shourabi Porcel <david@djsp.eu>
2022-12-19 12:29:19 +0100

    Enable Neovim tab navigation with , and .

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,21 @@ vim.api.nvim_set_option(
 	"ignorecase",
 	true
 )
+
+vim.api.nvim_set_keymap(
+	"n",
+	",",
+	":tabp<CR>",
+	{
+		noremap = true
+	}
+)
+
+vim.api.nvim_set_keymap(
+	"n",
+	".",
+	":tabn<CR>",
+	{
+		noremap = true
+	}
+)
