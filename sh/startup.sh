4cdd5f9d71eb285cf77ea6c5d141f807ff95cb46
David Joaquín Shourabi Porcel <david@djsp.eu>
2024-09-21 15:31:48 +0200

    Have `debsign` and `dpkg-sig` sign Debian packages with OpenPGP key `E0C3497126B72CA47975FC322953BB8C16043B43`

--- sh/startup.sh
+++ sh/startup.sh
@@ -7,13 +7,21 @@
 # https://specifications.freedesktop.org/basedir-spec/latest/
 AWS_CONFIG_FILE="${XDG_CONFIG_HOME:-${HOME}/.config}/aws/config.ini"
 
+# Sign Debian packages with the OpenPGP key identified by the following fingerprint.
+#
+# man:debsign(1)
+# man:dpkg-sig(1)
+DEBSIGN_KEYID=E0C3497126B72CA47975FC322953BB8C16043B43
+
 # Prevent changes to variables that are meant to stay set after this script is executed but should not be modified by accident.
 readonly \
 	-- \
 	AWS_CONFIG_FILE \
+	DEBSIGN_KEYID \
 	#
 
 export \
 	-- \
 	AWS_CONFIG_FILE \
+	DEBSIGN_KEYID \
 	#
