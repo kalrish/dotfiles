6a07236ea4e008b147c91e40fcbb4d79b1469a67
David Joaquín Shourabi Porcel <david@djsp.eu>
2024-09-21 21:52:50 +0200

    Display git branch in primary Bash prompt

--- bash/interactive.sh
+++ bash/interactive.sh
@@ -21,7 +21,70 @@ shopt \
 	histappend \
 	#
 
+function _check_directory
+{
+	if [[ "${PWD}" != "${_last_working_directory}" ]]
+	then
+		# The shell changed to a different directory.
+
+		_last_working_directory="${PWD}"
+
+		# The new working directory might be inside the work tree of a git repository.
+		_must_update_git_prompt=1
+	fi
+} &> /dev/null
+
+function _get_git_branch
+{
+	# man:git-rev-parse(1)
+	git \
+		rev-parse \
+		--symbolic-full-name \
+		--abbrev-ref \
+		HEAD \
+		#
+} 2> /dev/null
+
+function _trap_debug
+{
+	if [[ ${BASH_COMMAND} == git\ * ]]
+	then
+		# The next command starts with `git `, so it might check out a different branch.
+		_must_update_git_prompt=1
+	fi
+} &> /dev/null
+
+function _update_git_prompt
+{
+	if (( _must_update_git_prompt == 1 ))
+	then
+		if ! _git_branch="$(_get_git_branch)"
+		then
+			unset \
+				-- \
+				_git_branch \
+				#
+		fi
+		_must_update_git_prompt=0
+	fi
+} &> /dev/null
+
+readonly \
+	-f \
+	-- \
+	_check_directory \
+	_get_git_branch \
+	_trap_debug \
+	_update_git_prompt \
+	#
+
+declare \
+	-i \
+	-- \
+	_must_update_git_prompt \
+	#
 _exit_code_padding='   '
+_must_update_git_prompt=1
 
 # Save every command that does not begin with a space character in the history list.
 #
@@ -56,12 +119,23 @@ HISTSIZE=-1
 # man:strftime(3)
 HISTTIMEFORMAT='%FT%T%z '
 
+# Execute the functions named in the following indexed array before the primary prompt (`PS1`) is displayed.
+#
+# info:(bash)Bash_Variables
+# info:(bash)Controlling_the_Prompt
+# info:(bash)Interactive_Shell_Behavior
+PROMPT_COMMAND=(
+	_check_directory
+	_update_git_prompt
+)
+
 # Show the following information in the primary prompt:
 #
 # - the user's name (`\u`);
 # - whether they are root (`\$`);
 # - the hostname (`\H`);
 # - the current working directory (`\w`);
+# - the name of the checked out git branch (`${_git_branch}`), if any;
 # - the time in 24-hour HH:MM format (`\A`); and
 # - the exit code of the last command (`${?}`), padded with spaces to the width of three characters.
 #
@@ -72,7 +146,7 @@ HISTTIMEFORMAT='%FT%T%z '
 # info:(bash)Interactive_Shell_Behavior
 # info:(bash)Shell_Parameter_Expansion
 # info:(bash)Special_Parameters
-PS1='\u@\H \w\n\A ${_exit_code_padding:${#?}}${?} \$ '
+PS1='\u@\H \w${_git_branch:+ ${_git_branch}}\n\A ${_exit_code_padding:${#?}}${?} \$ '
 
 # Prevent changes to variables that:
 #
@@ -88,4 +162,11 @@ readonly \
 	HISTTIMEFORMAT \
 	PS1 \
 	_exit_code_padding \
+	#
+
+# Execute the function `_trap_debug` before every command.
+trap \
+	-- \
+	_trap_debug \
+	DEBUG \
 	#
