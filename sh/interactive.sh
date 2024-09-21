ad7623f4dafb8378bc1fcad1190661a440c6a520
David Joaquín Shourabi Porcel <david@djsp.eu>
2024-09-21 15:28:43 +0200

    Set environment variable `INPUTRC` in shell startup scripts instead of systemd user manager
    The environment variable `INPUTRC` is relevant only for the GNU Readline
    library, which is used exclusively by command-line shells. Therefore,
    the environment variable `INPUTRC` should be set by shell startup
    scripts.

--- sh/interactive.sh
+++ sh/interactive.sh
@@ -1,2 +1,19 @@
 # This script shall be executed by POSIX shells when started in interactive mode.
 
+
+# Have the GNU Readline library load its initialization file from the directory designated by the XDG Base Directory specification for user-specific configuration files.
+#
+# man:readline(3)
+# https://specifications.freedesktop.org/basedir-spec/latest/
+INPUTRC="${XDG_CONFIG_HOME:-${HOME}/.config}/readline/inputrc"
+
+# Prevent changes to variables that are meant to stay set after this script is executed but should not be modified.
+readonly \
+	-- \
+	INPUTRC \
+	#
+
+export \
+	-- \
+	INPUTRC \
+	#
