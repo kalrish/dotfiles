feed6b7085a059056e18541d0c4f424866b5a682
David Joaquín Shourabi Porcel <david@djsp.eu>
2024-09-21 15:30:15 +0200

    Have AWS CLI load configuration from `${XDG_CONFIG_HOME}`

--- sh/startup.sh
+++ sh/startup.sh
@@ -1,2 +1,19 @@
 # This script shall be executed by POSIX shells at startup.
 
+
+# Have the AWS CLI load its "less sensitive configuration options" from a file in the directory designated by the XDG Base Directory specification for user-specific configuration files.
+#
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files#cli-configure-files-where
+# https://specifications.freedesktop.org/basedir-spec/latest/
+AWS_CONFIG_FILE="${XDG_CONFIG_HOME:-${HOME}/.config}/aws/config.ini"
+
+# Prevent changes to variables that are meant to stay set after this script is executed but should not be modified by accident.
+readonly \
+	-- \
+	AWS_CONFIG_FILE \
+	#
+
+export \
+	-- \
+	AWS_CONFIG_FILE \
+	#
