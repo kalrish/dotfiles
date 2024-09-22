50b4b5a1f716f6aedd675711cc30cebe421f5873
David Joaquín Shourabi Porcel <david@djsp.eu>
2024-09-23 00:19:58 +0200

    Prevent accidental termination of interactive POSIX shells with Ctrl-D

--- sh/interactive.sh
+++ sh/interactive.sh
@@ -1,6 +1,16 @@
 # This script shall be executed by POSIX shells when started in interactive mode.
 
 
+# Prevent accidental termination with Ctrl-D.
+#
+# Ctrl-D is used in at least one application: Google Meet, where it mutes and unmutes one's audio input.
+# Sometimes, the focus is unknowingly on the terminal window and the keystroke gets sent to it, terminating the interactive shell session it ran.
+#
+# man:set(1p)
+set \
+	-o ignoreeof \
+	#
+
 # Have the GNU Readline library load its initialization file from the directory designated by the XDG Base Directory specification for user-specific configuration files.
 #
 # man:readline(3)
