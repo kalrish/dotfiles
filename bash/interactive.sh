6feb37a0a7ab632a217957994a31b7f33a0fc274
David Joaquín Shourabi Porcel <david@djsp.eu>
2024-09-22 17:56:58 +0200

    Have Bash append to its history file
    … instead of overwriting it.

--- bash/interactive.sh
+++ bash/interactive.sh
@@ -10,3 +10,13 @@ source \
 	-- \
 	"${XDG_CONFIG_HOME:-${HOME}/.config}/sh/interactive.sh" \
 	#
+
+# Append to the history file instead of overwriting it.
+#
+# info:(bash)The_Shopt_Builtin
+# info:(bash)Bash_History_Facilities
+shopt \
+	-s \
+	-- \
+	histappend \
+	#
